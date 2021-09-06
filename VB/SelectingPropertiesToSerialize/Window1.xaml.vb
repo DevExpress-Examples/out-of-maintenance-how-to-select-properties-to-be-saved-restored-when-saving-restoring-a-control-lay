24ad1c9e8789e73a9bbeb4f3f551e1ef30cd3bb6
DevExpressExampleBot <fakeEmail>
2021-09-07 00:56:58 +0300

    autogenerate VB

--- VB/SelectingPropertiesToSerialize/Window1.xaml.vb
+++ VB/SelectingPropertiesToSerialize/Window1.xaml.vb
@@ -1,5 +1,4 @@
-﻿Imports Microsoft.VisualBasic
-Imports System.Collections.Generic
+﻿Imports System.Collections.Generic
 Imports System.Windows
 Imports DevExpress.Xpf.Core.Serialization
 Imports DevExpress.Xpf.Grid
@@ -7,6 +6,7 @@ Imports DevExpress.Xpf.Grid
 Namespace SelectingPropertiesToSerialize
 	Partial Public Class Window1
 		Inherits Window
+
 		Public Sub New()
 			InitializeComponent()
 		End Sub
@@ -17,46 +17,38 @@ Namespace SelectingPropertiesToSerialize
 			Next column
 		End Sub
 		Private Sub column_AllowProperty(ByVal sender As Object, ByVal e As AllowPropertyEventArgs)
-			e.Allow = e.DependencyProperty Is GridColumn.ActualWidthProperty OrElse e.DependencyProperty Is GridColumn.FieldNameProperty OrElse e.DependencyProperty Is GridColumn.VisibleProperty
+			e.Allow = e.DependencyProperty = GridColumn.ActualWidthProperty OrElse e.DependencyProperty = GridColumn.FieldNameProperty OrElse e.DependencyProperty = GridColumn.VisibleProperty
 		End Sub
 		Public Class IssueList
 			Public Shared Function GetData() As List(Of IssueDataObject)
 				Dim data As New List(Of IssueDataObject)()
-				data.Add(New IssueDataObject() With {.IssueName = "Transaction History", .IssueType = "Bug", .IsPrivate = True})
-				data.Add(New IssueDataObject() With {.IssueName = "Ledger: Inconsistency", .IssueType = "Bug", .IsPrivate = False})
-				data.Add(New IssueDataObject() With {.IssueName = "Data Import", .IssueType = "Request", .IsPrivate = False})
-				data.Add(New IssueDataObject() With {.IssueName = "Data Archiving", .IssueType = "Request", .IsPrivate = True})
+				data.Add(New IssueDataObject() With {
+					.IssueName = "Transaction History",
+					.IssueType = "Bug",
+					.IsPrivate = True
+				})
+				data.Add(New IssueDataObject() With {
+					.IssueName = "Ledger: Inconsistency",
+					.IssueType = "Bug",
+					.IsPrivate = False
+				})
+				data.Add(New IssueDataObject() With {
+					.IssueName = "Data Import",
+					.IssueType = "Request",
+					.IsPrivate = False
+				})
+				data.Add(New IssueDataObject() With {
+					.IssueName = "Data Archiving",
+					.IssueType = "Request",
+					.IsPrivate = True
+				})
 				Return data
 			End Function
 		End Class
 		Public Class IssueDataObject
-			Private privateIssueName As String
 			Public Property IssueName() As String
-				Get
-					Return privateIssueName
-				End Get
-				Set(ByVal value As String)
-					privateIssueName = value
-				End Set
-			End Property
-			Private privateIssueType As String
 			Public Property IssueType() As String
-				Get
-					Return privateIssueType
-				End Get
-				Set(ByVal value As String)
-					privateIssueType = value
-				End Set
-			End Property
-			Private privateIsPrivate As Boolean
 			Public Property IsPrivate() As Boolean
-				Get
-					Return privateIsPrivate
-				End Get
-				Set(ByVal value As Boolean)
-					privateIsPrivate = value
-				End Set
-			End Property
 		End Class
 		Private Sub btnRestoreLayout_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
 			gridControl1.RestoreLayoutFromXml("layout.xml")
